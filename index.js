923b0f15395c6d220448ae895a971cda31a83f5e
wsam3 <90058851+wsam3@users.noreply.github.com>
2022-06-05 16:49:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client,Intents, Collection } = require("discord.js");
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MESSAGES ,Intents.FLAGS.GUILD_MESSAGE_REACTIONS,Intents.FLAGS.GUILD_VOICE_STATES] ,
+});
+module.exports = client;
+
+// Global Variables
+client.commands = new Collection();
+client.slashCommands = new Collection();
+client.config = require("./config.json");
+
+// Initializing the project
+require("./handler")(client);
+
+client.login(client.config.token);
